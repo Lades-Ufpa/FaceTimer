92127754b904bc2f6c204e80046da5fb2445cf4f
KennedySouza <kennedy.es.souza@gmail.com>
2018-04-06 20:52:38 -0300

    Correção na inicalização de contagem

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener(
         if (request.greeting == "hello"){
                 var time = 0;
                 chrome.storage.sync.get("time", function (items) {
-                    if(items.time == null && items.time == "" && typeof items.time  == 'undefined'){
+                    if(items.time == null || items.time == "" || typeof items.time  == 'undefined'){
                         alert("null");
                         chrome.storage.sync.set({time: 1 }, function () {
                             time=1;
@@ -24,6 +24,7 @@ chrome.runtime.onMessage.addListener(
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.storage.sync.get("time", function (items) {
     alert('Tempo gasto no Facebook: '+items.time);
+
     });
 });
 
