8817be376b02b2e1a74defcc3e286723c615a974
KennedySouza <kennedy.es.souza@gmail.com>
2018-04-12 21:45:12 -0300

    Time Loss Meter
    Uma nova extensão.

--- background.js
+++ background.js
@@ -1,18 +1,36 @@
+var domain="";
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10);
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.greeting == "hello"){
+                domain = '$Time_'+request.domain;
                 var time = 0;
-                chrome.storage.sync.get("time", function (items) {
-                    if(items.time == null || items.time == "" || typeof items.time  == 'undefined'){
-                        alert("null");
-                        chrome.storage.sync.set({time: 1 }, function () {
+                chrome.storage.sync.get(domain, function (items) {
+                    if(items[domain] == null || items[domain] == "" || typeof items[domain]  == 'undefined'){
+                        chrome.storage.sync.set({[domain]:1}, function () {
                             time=1;
                         });
                     }else
                     {
-                        time = items.time;
+                        time = items[domain];
                     }
-                    chrome.storage.sync.set({time: time+1}, function () {
+                    chrome.storage.sync.set({[domain]: time+1}, function () {
+                    });
+                    chrome.storage.sync.get(domain, function (items) {
+                        browser.browserAction.setTitle({title: 'Tempo gasto no '+domain.replace('$Time_','')+': '+String(items[domain]).toHHMMSS()});
+                    });
+                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                        chrome.tabs.sendMessage(sender.tab.id, {greeting: domain.replace('$Time_',''), time: String(items[domain]).toHHMMSS()}, function(response) {});  
                     });
                 });
         }
@@ -22,10 +40,8 @@ chrome.runtime.onMessage.addListener(
 
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.storage.sync.get("time", function (items) {
-    alert('Tempo gasto no Facebook: '+items.time);
+    chrome.storage.sync.get(domain, function (items) {
+    alert('Tempo gasto no '+domain.replace('$Time_','')+': '+String(items[domain]).toHHMMSS());
 
     });
 });
-
-alert("running");
--- content.js
+++ content.js
@@ -1,15 +1,62 @@
 var focused=false;
+var originalTitle="";
+function extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+function extractRootDomain(url) {
+    var domain = extractHostname(url),
+        splitArr = domain.split('.'),
+        arrLen = splitArr.length;
+
+    //extracting the root domain here
+    //if there is a subdomain 
+    if (arrLen > 2) {
+        domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+        //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
+        if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
+            //this is using a ccTLD
+            domain = splitArr[arrLen - 3] + '.' + domain;
+        }
+    }
+    return domain;
+}
+
 function sendMessage()
 {
     if(focused)
     {
-        chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-            //console.log(response.farewell);
+        chrome.runtime.sendMessage({greeting: "hello", domain: extractRootDomain(window.location.href)}, function(response) {
         });
     }
 }
 
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {      
+    if(request.greeting ==extractRootDomain(window.location.href))
+    {
+        document.title = request.time+" | "+originalTitle;
+    }
+}
+);
+
+
 function startTimer() {
+    originalTitle=document.title;
     var ticker = setInterval("sendMessage()", 1000);
 }
 
@@ -20,4 +67,8 @@ window.onblur = function() {
     focused = false;
 };
 
-startTimer();
+window.onload = function() {
+    startTimer();
+    focused = document.hasFocus();
+  };
+
